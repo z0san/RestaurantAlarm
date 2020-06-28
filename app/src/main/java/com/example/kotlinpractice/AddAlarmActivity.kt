8f0d5fbb999124445d98a717dbb9e40e94ceb13f
Zohar <zinger@seas.upenn.edu>
2020-06-28 22:25:50 +0100

    finished UI for now

--- app/src/main/java/com/example/kotlinpractice/AddAlarmActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/AddAlarmActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlinpractice
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.sasikanth.colorsheet.ColorSheet
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.add_alarm.*
 
@@ -15,6 +16,22 @@ class AddAlarmActivity: AppCompatActivity() {
         submitButton.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+        colorSelector.setOnClickListener {
+            val colors = intArrayOf(0xFF82B926.toInt(), 0xFFA276EB.toInt(),
+                0xFF6A3AB2.toInt(), 0xFF666666.toInt(), 0xFFFFFF00.toInt(),
+                0xFF3C8D2F.toInt(), 0xFFFA9F00.toInt(), 0xFFFF0000.toInt()
+            )
+
+            ColorSheet().colorPicker(
+                colors = colors,
+                listener = { color ->
+                    // Handle color
+                })
+                .show(supportFragmentManager)
+        }
+
+
     }
 
 
