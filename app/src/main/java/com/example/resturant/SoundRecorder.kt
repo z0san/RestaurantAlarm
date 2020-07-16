019e947b990f3284ebec1aef775e48afaee4b213
Cyrus Signer <japaneserhino@gmail.com>
2020-07-16 09:11:47 +0100

    add playback to new recording page

--- app/src/main/java/com/example/resturant/SoundRecorder.kt
+++ app/src/main/java/com/example/resturant/SoundRecorder.kt
@@ -3,23 +3,28 @@ package com.example.resturant
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnCompletionListener
 import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.sound_recorder.*
 import java.io.File
+
 
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class SoundRecorder: AppCompatActivity() {
-    var mediaRecorder: MediaRecorder = MediaRecorder()
+    private var mediaRecorder: MediaRecorder?=null
+    private var mediaPlayer: MediaPlayer?=null
 
     //permissions stuff
     private var permissionToRecordAccepted = false
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    private var playing: Boolean=false
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +34,7 @@ class SoundRecorder: AppCompatActivity() {
         endButton.isEnabled=false
         startButton.isEnabled=false
         saveButton.isEnabled=false
+        playButton.isEnabled=false
         //request permission
         requestPermissions( permissions, REQUEST_RECORD_AUDIO_PERMISSION)
 
@@ -36,24 +42,25 @@ class SoundRecorder: AppCompatActivity() {
 
 
             //setup media recorder
-            mediaRecorder = MediaRecorder()
-            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
-            mediaRecorder.setOutputFile(File(this.filesDir, "tempRecording.3gp"))
+            mediaRecorder= MediaRecorder()
+            mediaRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mediaRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            mediaRecorder!!.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
+            mediaRecorder!!.setOutputFile(File(this.filesDir, "tempRecording.3gp"))
             //record
-            mediaRecorder.prepare()
-            mediaRecorder.start()
+            mediaRecorder!!.prepare()
+            mediaRecorder!!.start()
 
             startButton.setEnabled(false)
             endButton.setEnabled(true)
         }
         endButton.setOnClickListener{
-            mediaRecorder.stop()
-            mediaRecorder.release()
+            mediaRecorder?.stop()
+            mediaRecorder?.release()
             endButton.isEnabled=false
             startButton.isEnabled=true
             saveButton.isEnabled=true
+            playButton.isEnabled=true
         }
         saveButton.setOnClickListener{
             if(checkName()){
@@ -63,6 +70,33 @@ class SoundRecorder: AppCompatActivity() {
         }
         cancelButton.setOnClickListener{
             startActivity(Intent(this, AddAlarmActivity::class.java))
+        }
+        playButton.setOnClickListener{
+            if(!playing) {
+                playButton.text="STOP"
+                mediaPlayer = MediaPlayer()
+
+                //set listener for finished
+                mediaPlayer!!.setOnCompletionListener(OnCompletionListener {
+                    //when finisehd player
+                    playButton.text="PLAY"
+                    mediaPlayer?.stop()
+                    mediaPlayer?.release()
+                    playing=false
+                })
+
+                //setup
+                mediaPlayer!!.setDataSource(File(this.filesDir, "tempRecording.3gp").absolutePath)
+                mediaPlayer!!.prepare()
+                mediaPlayer!!.start()
+            }
+            else
+            {
+                playButton.text="PLAY"
+                mediaPlayer?.stop()
+                mediaPlayer?.release()
+            }
+            playing=!playing
         }
     }
 
@@ -83,5 +117,13 @@ class SoundRecorder: AppCompatActivity() {
             false
         }
         if(permissionToRecordAccepted) startButton.isEnabled=true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaRecorder?.release()
+        mediaRecorder=null
+        mediaPlayer?.release()
+        mediaPlayer=null
     }
 }
